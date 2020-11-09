c30d269bfa388da2d98a26f758b30ea4f31560a0
唐某某 <tangmoumou@tangmoumoudeMacBook-Pro.local>
2020-11-09 15:12:53 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mUpdating seed for Canada Dream...\033[0m\n"
+
+# push your files to github
+msg="saving file and pushing to github $(date)"
+if [ -n "$*" ]; then
+        msg="$*"
+fi
+git add .
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+printf "upload success, enjoy your journey! "
